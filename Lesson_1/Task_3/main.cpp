4d2387c2a1806c0003862b831909e33564e9cfb1
eugene1beetle <eugene.beetle.01@gmail.com>
2020-11-10 23:02:27 +0200

    solved task 3

--- Lesson_1/Task_3/main.cpp
+++ Lesson_1/Task_3/main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int nutsNeed = 0, coneCollect = 0, nutsInCone = 0;
+
+    cout << "Squirrel needs some nuts to survive a winter." << endl;
+    cout << "How many nuts do she need?: ";
+    cin >> nutsNeed;
+    cout << "How many nuts in one cone?: ";
+    cin >> nutsInCone;
+    cout << "How many cone do squirrel collect?: ";
+    cin >> coneCollect;
+
+    if (nutsNeed <= coneCollect * nutsInCone) cout << "YES, She'll survive";
+    else cout << "NO, She needs more nuts to survive";
 }
