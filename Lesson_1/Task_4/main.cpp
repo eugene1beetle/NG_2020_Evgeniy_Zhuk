ddc10ce7292c651ecfd8d24f1f378a2e9098da21
eugene1beetle <eugene.beetle.01@gmail.com>
2020-11-10 23:12:55 +0200

    solved Task_4

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int salary = 0;
+
+    cout << "Enter your salary: ";
+    cin >> salary;
+    if (salary < 1000) cout << "You need to work more!";
+    else if (salary < 1000000) cout << "You have nice salary!";
+    else cout << "You're millioner!";
 }
