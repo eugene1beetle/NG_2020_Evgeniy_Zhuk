17fb954986eebf9bf970ead597d3588b3f4fcfbc
eugene1beetle <eugene.beetle.01@gmail.com>
2020-11-11 05:24:58 +0200

    solved task 8

--- Lesson_1/Task_8/main.cpp
+++ Lesson_1/Task_8/main.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int threeSize = 0, counter = 1;
+    cout << "Enter christmas three size: ";
+    cin >> threeSize;
+    cout << endl;
+    for (int i = threeSize; i > 0; i--)
+    {
+        for (int j = i - 1; j > 0; j--)
+        {
+            cout << " ";
+        }
+        for (int j = counter; j > 0; j--)
+        {
+            cout << "*";
+        }
+        counter += 2;
+        cout << endl;
+    }
+
+    for (int i = 0; i < threeSize - 1; i++)
+    {
+        cout << " ";
+    }
+    cout << "*" << endl;
 }
