b80608419c6fec59812d4b8ecc324ec4f484f15e
eugene1beetle <eugene.beetle.01@gmail.com>
2020-11-12 01:43:20 +0200

    git add *

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 int main()
 {
-
+    cout << "Once... after hard work our hero decides to go in a real man's relax, that is some exercises in the gym.\n";
+    cout << "The hero comes to his house and take some leather staff that he loved to wear\n";
+    cout << "-Now I'm ready to go, yeah!\n";
+    cout << "When the hero comes closer to the gym, he started feel some fight aura, but he isn't scared of it\n";
+    cout << "-OK, i'm going in";
+    cout << ""
 }
