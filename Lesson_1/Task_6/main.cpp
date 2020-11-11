0f3f236ef5625d216accccb22aac6d8bcb8dd4fa
eugene1beetle <eugene.beetle.01@gmail.com>
2020-11-11 04:55:27 +0200

    solved task 6

--- Lesson_1/Task_6/main.cpp
+++ Lesson_1/Task_6/main.cpp
@@ -4,6 +4,62 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int number = 0;
+    cout << "Enter some number for painting: ";
+    cin >> number;
+
+    for (int i = number; i > 0; i--)
+    {
+        for (int j = number; j > 0; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < number + 1; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < number + 1; i++) // ocey
+    {
+        for (int j = number - i; j > 0; j--)
+        {
+            cout << " ";
+        }
+        for (int j = i; j > 0; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int i = number; i > 0; i--)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int i = 0; i < number + 1; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            cout << " ";
+        }
+        for (int j = number - i; j > 0; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
