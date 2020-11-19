7d024c25419e709f8c28c7a8ebbf54e883f99c77
eugene1beetle <eugene.beetle.01@gmail.com>
2020-11-20 01:42:06 +0200

    added lesson 2 folder

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -4,10 +4,26 @@ using namespace std;
 
 int main()
 {
+    int firstChoice = 0;
+    cout << "Boss of this gym\nPress enter to continue\n";
+    getchar();
     cout << "Once... after hard work our hero decides to go in a real man's relax, that is some exercises in the gym.\n";
+    getchar();
     cout << "The hero comes to his house and take some leather staff that he loved to wear\n";
+    getchar();
     cout << "-Now I'm ready to go, yeah!\n";
+    getchar();
     cout << "When the hero comes closer to the gym, he started feel some fight aura, but he isn't scared of it\n";
+    getchar();
     cout << "-OK, i'm going in";
-    cout << ""
+    getchar();
+    cout << "He comes closer to his locker and some stranger in white t-shirt say to him:";
+    getchar();
+    cout << "-Hey buddy, I think you've got the wrong door, the leather club's two blocks down.";
+    getchar();
+    cout << "But the hero was not confused, he really liked that leather cloth and can't stand it, so he says:";
+    getchar();
+    cout << "1 - Why you bully me, you look like nice guy";
+    cout << "2 - No, i don't think so, i get the right door";
+    cout << "3 - F##k you...";
 }
