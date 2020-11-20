32afb893e10daae460843c1b153606a5936b0f99
eugene1beetle <eugene.beetle.01@gmail.com>
2020-11-20 04:11:14 +0200

    solved task 4

--- Lesson_2/Task_4/main.cpp
+++ Lesson_2/Task_4/main.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int numbers[5];
+    int maxNumber = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Stars in " << i + 1 << " column: ";
+        cin >> numbers[i];
+        if (numbers[i] > maxNumber) maxNumber = numbers[i];
+    }
+
+    for (int i = 0; i < maxNumber; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (numbers[j] > 0)
+            {
+                cout << "*";
+                numbers[j]--;
+            }
+            else cout << " ";
+        }
+        cout << endl;
+    }
 }
