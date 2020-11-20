ca477cb9d3874d95f17ecf03a70f1164bca1b707
eugene1beetle <eugene.beetle.01@gmail.com>
2020-11-20 03:49:18 +0200

    solved 3 task

--- Lesson_2/Task_3/main.cpp
+++ Lesson_2/Task_3/main.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int numbers[20];
+    int maxNumber = 0;
+
+    for (int i = 0; i < 20; i++)
+    {
+        cout << "Enter number: ";
+        cin >> numbers[i];
+        if (numbers[i] > maxNumber) maxNumber = numbers[i];
+        if (numbers[i] == 0) break;
+    }
+
+    for (int i = 0; i < 20 && numbers[i] != 0; i++)
+    {
+        for (int j = 0; j < (maxNumber / 2 + 1) - (numbers[i] / 2 + 1); j++)
+            cout << " ";
+
+        for (int j = 0; j < numbers[i]; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
 }
